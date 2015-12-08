8cce389ce8227dfb50d54e7ec4da7803d09e82bb
suroor <surusuroor@gmail.com>
2015-12-08 22:40:35 +0530

    Adds gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup( name ='Hoso',
+       version = '0.1',
+       description = 'Social media integrator',
+       url ='https://github.com/suroorhussain/Hoso',
+       author = 'Team Hoso',
+       author_email='surusuroor@gmail.com',
+       license='MIT',
+       packages=['Hwacha'],
+       install_requires=['tweepy', 'facebook-sdk'],
+       zip_safe=False
+       )
