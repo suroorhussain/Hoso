49655e1d6345430695bf14e414df4e141fad3e32
Mohamed Jassim <jassim.al.azmi@gmail.com>
2015-12-10 05:18:12 +0300

    Adds error code printing

--- trials/twitter_api.py
+++ trials/twitter_api.py
@@ -1,15 +1,13 @@
 import tweepy
 import message
-#import urllib3
-
-#urllib3.disable_warnings()
-
-#from sys import argv
 
 def get_api(cfg):
   auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
   auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
   return tweepy.API(auth)
+
+def get_tweet():
+  return message.get_message()
 
 def main():
   # Fill in the values noted in previous step here
@@ -19,11 +17,17 @@ def main():
     "access_token"        : "4405674753-nnx6NeJhG7oqDptByGNAeRA6ozFCTTtQFmQLYX6",
     "access_token_secret" : "pOqpS7r7aKG2YcOsMF9dtWHbFXcvADSfo4TDaIVzBIOrY" 
     }
-
+  error = 0
   api = get_api(cfg)
-  tweet = message.get_message()
-  status = api.update_status(status=tweet) 
-  # Yes, tweet is called 'status' rather confusing
+  tweet = get_tweet()
+  try: 
+    status = api.update_status(status=tweet) 
+  except tweepy.TweepError as e:
+    error = e[0][0]['code']
+  
+  print "error code"
+  print error
+# Yes, tweet is called 'status' rather confusing
 
 if __name__ == "__main__":
   main()
