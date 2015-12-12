6eb6d846884e417323b1638287c3500eee3ea3e4
suroor <surusuroor@gmail.com>
2015-12-13 00:23:49 +0530

    Adds error handling to facebook broadcast

--- hoso/channels.py
+++ hoso/channels.py
@@ -13,15 +13,33 @@ class channel(object): #Abstract class for all channels
 
 
 class fb(channel): #Class for facebook
+
     def __init__(self, token, message):
         self.access_token = token
         self.post = message
+        self.status = "Success"
+        self.error_code = 0
         
     def broadcast(self):
-        graph = facebook.GraphAPI(self.access_token)
-        return graph.put_object("me", "feed", message = self.post)
+        try:
+            
+            print "starting broadcast"
+            graph = facebook.GraphAPI(self.access_token)
+            graph.put_object("me", "feed", message = self.post)
+            self.error_code = 1
+            
+        except facebook.GraphAPIError as err: 
+            self.error_code = -1
+            if 'expired' in err[0]:
+                self.status = "Token expired"
+            elif 'status' in err[0]:
+                self.status = "Duplicate message"
+            elif 'limit reached' in err[0]:
+                self.status = "Feed limit reached. Please Try again after 24 hours"
 
+                
 class twitter(channel): #Class for twitter
+
     def __init__(self, consumer_key, consumer_secret, access_token,access_token_secret, message):
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
@@ -46,6 +64,8 @@ class twitter(channel): #Class for twitter
             error_code = e[0][0]['code']
             #print error_message
             return error_message
+
+
 class mail(channel): #Class for mail
     
     def __init__(self,sender,text_message):
