59e2010500c73f732bd44168c655be7b27458740
abhijithcnair <abhijithcnair@yahoo.com>
2015-12-15 19:21:09 +0530

    Test added for the broadcast function fixes#15.

--- hoso/test_mail.py
+++ hoso/test_mail.py
@@ -10,4 +10,13 @@ def test_authentication():
    login = mock.SendGridClient(username, password)
    mock.login = "True"
    mock.authentication.assert_called_with()
+
+def test_broadcast():
+   mock_server = Mock()
+   mock_server.broadcast()
+   To = "hoso@gmail.com"
+   Message = "hai all"
+   return_value = mock_server.mail( To, Message)
+   mock_server.return_value == "mail sent"
+   mock_server.broadcast.assert_called_with()
    
