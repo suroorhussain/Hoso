86d3136d01ee439c9294bcd32133cefa37ebf009
suroor <surusuroor@gmail.com>
2015-12-17 21:12:25 +0530

    Removes facebook from channels

--- hoso/channels.py
+++ hoso/channels.py
@@ -1,9 +1,7 @@
-import facebook
 import tweepy
 import sendgrid
 from sendgrid import SendGridError, SendGridClientError, SendGridServerError
 import os
-
 
 
 class channel(object): #Abstract class for all channels
@@ -12,32 +10,6 @@ class channel(object): #Abstract class for all channels
 
     def broadcast(self):
         raise NotImplementedError
-
-
-class fb(channel): #Class for facebook
-
-    def __init__(self, token, message):
-        self.access_token = token
-        self.post = message
-        self.status = "Success"
-        self.error_code = 0
-        
-    def broadcast(self):
-        try:
-            
-            print "starting broadcast"
-            graph = facebook.GraphAPI(self.access_token)
-            graph.put_object("me", "feed", message = self.post)
-            self.error_code = 1
-            
-        except facebook.GraphAPIError as err: 
-            self.error_code = -1
-            if 'expired' in err[0]:
-                self.status = "Token expired"
-            elif 'status' in err[0]:
-                self.status = "Duplicate message"
-            elif 'limit reached' in err[0]:
-                self.status = "Feed limit reached. Please Try again after 24 hours"
 
                 
 class twitter(channel): #Class for twitter
@@ -103,12 +75,6 @@ class mail(channel): #Class for mail
             return "Cannot send the mail"
            
         
-    
-
-   
-    
-
-
 def twitter_api(cfg,message):     
     consumer_key = cfg['consumer_key']
     consumer_secret = cfg['consumer_secret']
