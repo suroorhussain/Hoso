5a1851f58411ef5d0de81a582fccffdbff74e0ab
Mohamed Jassim <jassim.al.azmi@gmail.com>
2015-12-19 16:20:44 +0300

    Adds init module for class twitter in channels

--- hoso/channels.py
+++ hoso/channels.py
@@ -14,7 +14,11 @@ class channel(object): #Abstract class for all channels
                 
 class twitter(channel): #Class for twitter
 
-
+    def __init__(self, consumer_key, consumer_secret, access_token,access_token_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
 
 class mail(channel): #Class for mail
     
