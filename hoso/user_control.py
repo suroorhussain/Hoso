5c7edf68860ac6047a433870550580921fadb287
Mohamed Jassim <jassim.al.azmi@gmail.com>
2015-12-24 19:48:28 +0300

    Adds message.py and user_control.py to hoso directory

---
+++ hoso/user_control.py
@@ -0,0 +1,14 @@
+
+class user_control():
+    
+    def available_channels():
+        raise NotImplementedError
+    
+    def message():
+        raise NotImplementedError
+        
+    def selected_channels():
+        raise NotImplementedError
+
+class default_user():
+    pass
