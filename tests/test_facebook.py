a7b7db5a78ec9157b3a53d4197a12c0a660f347e
suroor <surusuroor@gmail.com>
2015-12-26 21:17:24 +0530

    Facebook broadcast tests fail after changes are made according to guidelines in #27

--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -33,30 +33,30 @@ def test_facebook_authenticate_fail():
     facebook.GraphAPI = original_graph
         
     
-# def test_facebook_broadcast_success():
-#     original_graph = facebook.GraphAPI
-#     facebook.GraphAPI = mock.Mock(original_graph)
-#     graph_mock = mock.Mock(original_graph)
-#     facebook.GraphAPI.return_value = graph_mock
+def test_facebook_broadcast_success():
+    original_graph = facebook.GraphAPI
+    facebook.GraphAPI = mock.Mock(original_graph)
+    graph_mock = mock.Mock(original_graph)
+    facebook.GraphAPI.return_value = graph_mock
     
-#     fb = facebook_api.Facebook("token")
-#     fb.broadcast("my post")
+    fb = facebook_api.Facebook("token")
+    fb.broadcast("my post")
 
-#     facebook.GraphAPI.assert_called_with("token")
-#     graph_mock.put_object.assert_called_with("me", "feed", message = "my post")
+    facebook.GraphAPI.assert_called_with("token")
+    graph_mock.put_object.assert_called_with("me", "feed", message = "my post")
 
-#     facebook.GraphAPI = original_graph
+    facebook.GraphAPI = original_graph
 
-# def test_facebook_broadcast_error():
-#     original_graph = facebook.GraphAPI
-#     facebook.GraphAPI = mock.Mock(original_graph)
-#     graph_mock = mock.Mock(original_graph)
-#     facebook.GraphAPI.return_value = graph_mock
-#     graph_mock.put_object.side_effect = facebook.GraphAPIError({"error_description":"Error while broadcasting"})
+def test_facebook_broadcast_error():
+    original_graph = facebook.GraphAPI
+    facebook.GraphAPI = mock.Mock(original_graph)
+    graph_mock = mock.Mock(original_graph)
+    facebook.GraphAPI.return_value = graph_mock
+    graph_mock.put_object.side_effect = facebook.GraphAPIError({"error_description":"Error while broadcasting"})
 
-#     fb = facebook_api.Facebook("token")
-#     with pytest.raises(facebook_api.ChannelError):
-#         fb.broadcast("my post")
+    fb = facebook_api.Facebook("token")
+    with pytest.raises(facebook_api.ChannelError):
+        fb.broadcast("my post")
         
-#     facebook.GraphAPI = original_graph
+    facebook.GraphAPI = original_graph
 
