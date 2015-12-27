7d17cdbfd6bb2551a39f5c13a34dc847bc351d45
Mohamed Jassim <jassim.al.azmi@gmail.com>
2015-12-27 22:10:56 +0300

    Adds test for get credentials module of twitter

--- tests/test_twitter_channel.py
+++ tests/test_twitter_channel.py
@@ -1,7 +1,32 @@
 import hoso.channels as channels
 import mock
 import tweepy
+import __builtin__
 
+def test_raw_input(monkeypatch):
+    def mock_raw_input():
+         mock_consumer_key = mock.Mock()
+         mock_consumer_secret = mock.Mock()
+         mock_access_token = mock.Mock()
+         mock_access_token_secret = mock.Mock()
+
+         mock_consumer_key.return_value = 'key1'
+         mock_consumer_secret.return_value = 'key2'
+         mock_access_token.return_value = 'key3'
+         mock_access_token_secret.return_value = 'key4'
+
+         mock_dict = mock.Mock()
+         mock_dict.return_value = {'consumer_key': mock_consumer_key.return_value, 'consumer_secret' : mock_consumer_secret.return_value, 'access_token' : mock_access_token.return_value, 'access_token_secret' : mock_access_token_secret.return_value}
+         return mock_dict.return_value
+    
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    retval = raw_input()
+    
+    assert retval == {'consumer_key' : 'key1', 'consumer_secret':'key2', 'access_token' : 'key3', 'access_token_secret' : 'key4'}
+    
+    
+
+'''
 def test_auth():
     mock_OAuthHandler = mock.Mock()
     mock_auth = mock.Mock()
@@ -42,3 +67,4 @@ def test_broadcast():
     t.broadcast('msg')
     mock_api.update_status.assert_called_with(status="msg")
     
+'''
