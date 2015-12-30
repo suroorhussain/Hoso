bfdea028ca02497263e0958a2ad93d98967b7ba0
suroor <surusuroor@gmail.com>
2015-12-30 19:17:53 +0530

    Adds test files for user_control

---
+++ tests/test_user.py
@@ -0,0 +1,19 @@
+import hoso.user_control
+
+def test_my_channels():
+    pass
+
+def test_add_channel():
+    pass
+
+def test_remove_channel():
+    pass
+
+def test_select_channel():
+    pass
+
+def test_send_message():
+    pass
+
+def test_logout():
+    pass
---
+++ tests/test_user_control.py
@@ -0,0 +1,8 @@
+import hoso.user_control
+
+def test_add_user_new():
+    pass
+
+def test_add_user_existing():
+    pass
+
