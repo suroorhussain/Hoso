b75692b1dec4a443a7522f2d5067dd49b1977f41
Mohamed Jassim <jassim.al.azmi@gmail.com>
2015-12-30 05:41:01 +0300

    Modifies test of twitter authenticate function

--- tests/test_twitter_channel.py
+++ tests/test_twitter_channel.py
@@ -2,7 +2,6 @@ import hoso.channels as channels
 import mock
 import tweepy
 import __builtin__
-import pytest
 
 def test_raw_input(monkeypatch):
     def mock_raw_input():
@@ -51,18 +50,26 @@ def test_auth():
     mock_OAuthHandler.assert_called_with("test_consumer_key", "test_consumer_secret")
     mock_auth.set_access_token.assert_called_with("test_access_token","test_access_token_secret")
     mock_API.assert_called_with(mock_auth)
-    
+    assert t.consumer_key == 'test_consumer_key'
+    assert t.consumer_secret == 'test_consumer_secret'
+    assert t.access_token == 'test_access_token'
+    assert t.access_token_secret == 'test_access_token_secret'
+
     tweepy.OAuthHandler = original_OAuthHandler
     tweepy.API = original_API
 '''   
 def test_broadcast():
+    mock_OAuthHandler = mock.Mock()
+    mock_auth = mock.Mock()
+    mock_API = mock.Mock()
     mock_api = mock.Mock()
-    t = channels.Twitter("test_consumer_key", 
-                "test_consumer_secret", 
-                "test_access_token",
-                "test_access_token_secret")
-    t.authenticate = mock.Mock()
-    t.authenticate.return_value = mock_api
+
+    original_OAuthHandler = tweepy.OAuthHandler
+    original_API = tweepy.API
+    tweepy.OAuthHandler = mock_OAuthHandler
+    tweepy.API = mock_API
+
+    t = channels.Twitter()
     t.broadcast('msg')
     mock_api.update_status.assert_called_with(status="msg")
     
