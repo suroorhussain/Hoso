16ee58d5b8ac53044cdc4c3f9efdb3d9ae5acb8b
Mohamed Jassim <jassim.al.azmi@gmail.com>
2016-01-01 04:28:19 +0300

    Modifies code at add_channel and remove_channel in user_control.
    Here add_channel simply adds channel being passed to it using append and remove_channel simply removes the channel being passed to it using remove.
    Test Passes.

--- hoso/user_control.py
+++ hoso/user_control.py
@@ -1,4 +1,4 @@
-import hoso.channels
+from hoso import channels
 import pickle
 import os.path
 
@@ -11,34 +11,48 @@ class User(object):
             self.password = user_data['password']
             self.registered_channels = user_data['registered_channels']
             self.credentials = user_data['credentials']
-            
+            self.auth_errors = ['']
+            self.broadcast_errors = ['']
     def my_channels(self):
         return self.registered_channels
 
-    def remove_channel(self, channel_name):
-        registered_channels = self.registered_channels
-        credentials = self.credentials
-        
-        registered_channels.remove(channel_name)
-        del credentials[channel_name]
+    def add_channel(self, channel_name):
+        #if channel_name not in self.registered_channels:
+            self.registered_channels.append(channel_name)
+        #else:
+          #  raise duplicateChannel(channel_name)
+            
+      #  channel = getattr(channels, channel_name)()
+       # channel.get_credentials()
 
-        self.registered_channels = registered_channels
-        self.credentials = credentials
-   
-    def add_channel(self, channel_name, credentials):
-        self.registered_channels.append(channel_name)
-        self.credentials.update(credentials)
-        
+    def remove_channel(self, channel_name):
+        self.registered_channels.remove(channel_name)
+
     def select_channel(self, channel_name):
         raise NotImplementedError
 
     def send_message(self, message):
-        def send_message(self, message):
-         for media in self.selected_channels:
-            channel = getattr(hoso.channels, media)()
-            channel.authenticate(self.credentials[media])
-            channel.broadcast(message)
+        for media in self.selected_channels:
+            channel = getattr(channels, media)()
+            try:
+                channel.authenticate(self.credentials[media])
+            except channels.ChannelError as e:
+                self.auth_errors.append(media+':'+e.message)
 
+            try:
+                channel.broadcast(message)
+            except channels.ChannelError as e:
+                self.broadcast_errors.append(media+':'+e.message)
+
+        if self.auth_errors != ['']:
+            auth_error = self.auth_errors
+            self.auth_errors = ['']
+            raise AuthenticationError(','.join(auth_error))
+        elif self.broadcast_errors != ['']:
+            broadcast_error = self.broadcast_errors
+            self.broadcast_errors = ['']
+            raise BroadcastError(','.join(broadcast_error))
+            
     def save_user_data(self):
         raise NotImplementedError
 
@@ -46,6 +60,21 @@ class User(object):
 def add_user(username, password):
     raise NotImplementedError
 
+class duplicateChannel(Exception):
+    
+    def __init__(self, message):
+        self.message = message
+        Exception.__init__(self, self.message)
 
 
+class AuthenticationError(Exception):
+    
+    def __init__(self, message):
+        self.message = message
+        Exception.__init__(self, self.message)
 
+class BroadcastError(Exception):
+    
+    def __init__(self, message):
+        self.message = message
+        Exception.__init__(self, self.message)
