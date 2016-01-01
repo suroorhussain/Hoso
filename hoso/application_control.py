e7a89c84074414329328f4200ae1cc3eb0ae3d0c
suroor <surusuroor@gmail.com>
2016-01-02 02:33:47 +0530

    Adds skeleton for application control and test

---
+++ hoso/application_control.py
@@ -0,0 +1,19 @@
+from hoso import user_control
+
+def login(username, password):
+    pass
+
+def register(username, password):
+    pass
+
+def get_all_channels():
+    pass
+
+def select_channels(channel_list):
+    pass
+
+def view_selected_channels():
+    pass
+
+def deselect(channel):
+    pass
---
+++ tests/test_app_control.py
@@ -0,0 +1,31 @@
+from hoso import application_control
+
+def test_login():
+    pass
+
+def test_login_wronguser():
+    pass
+
+def test_login_wrongpass():
+    pass
+
+def test_register():
+    pass
+
+def test_register_existing_user():
+    pass
+
+def test_get_all_channels():
+    pass
+
+def test_select_channels():
+    pass
+
+def test_view_selected_channels():
+    pass
+
+def test_deselect():
+    pass
+
+def test_deselect_notesxisting():
+    pass
