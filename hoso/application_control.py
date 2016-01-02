9a06718d5cb8829aefccba03472921f3bcd5b4b2
Mohamed Jassim <jassim.al.azmi@gmail.com>
2016-01-02 16:27:02 +0300

    Defines selected_channels as an empty list

--- hoso/application_control.py
+++ hoso/application_control.py
@@ -1,6 +1,8 @@
 from hoso import user_control
 
 selected_channels = []
+user_name = 'test_user'
+user_ob = user_control.User(user_name)
 
 def login(username, password):
     pass
@@ -11,7 +13,7 @@ def register(username, password):
 def get_all_channels():
     pass
 
-def select_channels(channel_list, user_control.User()):
+def select_channels(channel_list, ob):
     pass
 
 def view_selected_channels():
