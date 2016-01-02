27250b3e538db9134656423303fa166d65471c92
Mohamed Jassim <jassim.al.azmi@gmail.com>
2016-01-02 15:07:52 +0300

    Adds code to view selected channels and updates the parameter passed to select_channel.

--- hoso/application_control.py
+++ hoso/application_control.py
@@ -1,5 +1,7 @@
 from hoso import user_control
 
+selected_channels = []
+
 def login(username, password):
     pass
 
@@ -9,11 +11,11 @@ def register(username, password):
 def get_all_channels():
     pass
 
-def select_channels(channel_list):
+def select_channels(channel_list, user_control.User()):
     pass
 
 def view_selected_channels():
-    pass
+    return selected_channels
 
-def deselect(channel):
+def deselect(channel_name):
     pass
