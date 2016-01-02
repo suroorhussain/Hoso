06200cead07891004a72da296b581737eff6dcdc
Mohamed Jassim <jassim.al.azmi@gmail.com>
2016-01-02 16:58:06 +0300

    Adds code to raise channel_name_error when trying to remove channel that does not exit.

--- hoso/application_control.py
+++ hoso/application_control.py
@@ -20,4 +20,13 @@ def view_selected_channels():
     return selected_channels
 
 def deselect(channel_name):
-    selected_channels.remove(channel_name)
+    if channel_name in selected_channels:
+        selected_channels.remove(channel_name)
+    else:
+        raise Channel_name_Error('Channel does not exist')
+
+class Channel_name_Error(Exception):
+    
+    def __init__(self, message):
+        self.message = message
+        Exception.__init__(self, self.message)
