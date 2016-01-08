81aecc6933b9e4bb675fee1f7f960dc19d75e27d
suroor <surusuroor@gmail.com>
2016-01-08 20:16:12 +0530

    Adds the terminal UI

---
+++ hoso/hoso_terminal.py
@@ -0,0 +1,44 @@
+import argparser
+import application_control
+
+def get_channels():
+    return raw_input("Please enter the channels seperated by commas: ").split(',')
+
+def get_msg():
+    return raw_input("Please enter your status update: ")
+
+
+if argparser.args.message is not None:
+    message = argparser.args.message
+else:
+    message = get_msg()
+
+if argparser.args.channels is not None:
+    selected_channels = argparser.args.channels
+else:
+    selected_channels = get_channels() 
+
+user = application_control.login('terminal_user', 'password')
+while True:
+    application_control.select_channels(selected_channels, user)
+    user.send_message(message, application_control.selected_channels)
+
+    c = raw_input("Would you like to make another update? (y/n): ")
+
+    if c == 'y':
+        print "1.use same channels \t 2.select new channels"
+        action = raw_input('>>>')
+        if action == '1':
+            print "posting to {}".format(','.join(application_control.selected_channels))
+        elif action == '2':
+            selected_channels = get_channels()
+
+        message = get_msg()
+    else:
+        break
+
+for channel in user.registered_channels:
+    user.remove_channel(channel)
+
+application_control.logout(user)
+            
